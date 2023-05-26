177a23747b2eda1022d06c4351798f0c52c8c85c
Emanuel Avila <76269418+meiazero@users.noreply.github>
2023-05-25 22:17:16 -0300

    fix: map not rendering.

--- src/app/denuncia/detalhes/index.public.jsx
+++
@@ -1,3 +0,0 @@
-export default function Id([params]) {
-    return <div>Id: {params.id}</div>
-}
--- src/app/mapa/page.jsx
+++ src/app/mapa/page.jsx
@@ -1,37 +1,17 @@
 "use client"
 
-import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-import "primeicons/primeicons.css"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import dynamic from "next/dynamic"
+
+const DynamicMap = dynamic(() => import("../components/Map"), { ssr: false })
 
 export default function Mapa() {
-    const position = [-3.6836, -39.5816]
-    const pin = L.icon({
-        iconUrl: "pinmap.svg",
-        iconSize: [32, 32]
-        // iconAnchor: [24, 48],
-        // popupAnchor: [0, -48]
-    })
-
     return (
-        <div id='page-map'>
-            <MapContainer center={position} zoom={15} scrollWheelZoom={true}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                />
-                <Marker position={position} icon={pin}>
-                    <Popup
-                        closeButton={false}
-                        minWidth={240}
-                        maxWidth={240}
-                        className='map-popup'
-                    >
-                        <h3>title</h3>
-                        <p>description</p>
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </div>
+        <>
+            <Header />
+            <DynamicMap />
+            <Footer />
+        </>
     )
 }
