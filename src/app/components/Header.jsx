a88495b64d08464b8ad8fe13273dd6432d85ce07
Emanuel Avila <76269418+meiazero@users.noreply.github>
2023-06-10 17:31:03 -0300

    feat: cadastro consumer API to create user account

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,14 @@
 export default function Header() {
     return (
-        <header className='flex items-center justify-between bg-gray-900 py-5 px-6'>
-            <div className='text-white text-3xl font-bold'>Itareport</div>
-            <nav className='space-x-5'>
-                <a href='/login' className='text-white'>
+        <header className="flex items-center justify-between bg-gray-900 py-5 px-6">
+            <div className="text-white text-3xl font-bold">Itareport</div>
+            <nav className="space-x-5">
+                <a href="/login" className="text-white">
                     logar
                 </a>
                 <a
-                    href='#'
-                    className='text-white rounded-md border border-slate-100 p-2 hover:bg-slate-100 hover:text-gray-900 transition duration-100 ease-in-out'
+                    href="/cadastrar"
+                    className="text-white rounded-md border border-slate-100 p-2 hover:bg-slate-100 hover:text-gray-900 transition duration-100 ease-in-out"
                 >
                     cadastrar
                 </a>
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { React, useState } from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
-export default function LoginForm() {
+export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
@@ -17,59 +19,63 @@ export default function LoginForm() {
     }
 
     return (
-        <div className='max-w-2xl mx-auto mt-40 py-3 bg-white rounded-md'>
-            <h1 className='text-4xl text-center font-semibold mb-5 mt-2 text-gray-700'>
-                Login
-            </h1>
-            <form
-                onSubmit={handleSubmit}
-                className='px-8 pt-6 pb-8 mb-4'
-                method='POST'
-                autoComplete='off'
-            >
-                <div className='mb-4'>
-                    <label
-                        className='block text-gray-700 text-2xl mb-4'
-                        htmlFor='email'
-                    >
-                        E-mail
-                    </label>
-                    <input
-                        type='email'
-                        id='email'
-                        className='shadow appearance-none border rounded-sm w-full py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                        placeholder='Digite seu e-mail'
-                        value={email}
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className='mb-6'>
-                    <label
-                        className='block text-gray-700 text-2xl mb-4'
-                        htmlFor='password'
-                    >
-                        Senha
-                    </label>
-                    <input
-                        type='password'
-                        id='password'
-                        className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                        placeholder='Digite sua senha'
-                        value={password}
-                        required
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className='flex items-center justify-end '>
-                    <button
-                        type='submit'
-                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-                    >
-                        Entrar
-                    </button>
-                </div>
-            </form>
-        </div>
+        <>
+            <Header />
+            <div className="max-w-2xl mx-auto my-5 py-3 bg-white rounded-md">
+                <h1 className="text-4xl text-center font-semibold mb-5 mt-2 text-gray-700">
+                    Login
+                </h1>
+                <form
+                    onSubmit={handleSubmit}
+                    className="px-8 pt-6 pb-8 mb-4"
+                    method="POST"
+                    autoComplete="off"
+                >
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 text-2xl mb-4"
+                            htmlFor="email"
+                        >
+                            E-mail
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="shadow appearance-none border rounded-sm w-full py-2 px-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            placeholder="Digite seu e-mail"
+                            value={email}
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            className="block text-gray-700 text-2xl mb-4"
+                            htmlFor="password"
+                        >
+                            Senha
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            placeholder="Digite sua senha"
+                            value={password}
+                            required
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex items-center justify-end ">
+                        <button
+                            type="submit"
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        >
+                            Entrar
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <Footer />
+        </>
     )
 }
