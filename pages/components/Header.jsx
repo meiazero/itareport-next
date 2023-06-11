5f88513360c7894404317dcbf44bef059e575fbf
Emanuel Avila <76269418+meiazero@users.noreply.github>
2023-06-11 17:24:44 -0300

    fix: use Link of Nx

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,17 +1,18 @@
+import Link from "next/link"
+
 export default function Header() {
     return (
         <header className="flex items-center justify-between bg-gray-900 py-5 px-6">
             <div className="text-white text-3xl font-bold">Itareport</div>
             <nav className="space-x-5">
-                <a href="/login" className="text-white">
+                <Link href="/login" className="text-white">
                     logar
-                </a>
-                <a
+                </Link>
+
+                <Link
                     href="/cadastrar"
                     className="text-white rounded-md border border-slate-100 p-2 hover:bg-slate-100 hover:text-gray-900 transition duration-100 ease-in-out"
-                >
-                    cadastrar
-                </a>
+                ></Link>
             </nav>
         </header>
     )
