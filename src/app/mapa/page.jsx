19ca2ec293bac3fc145a7ad99b5813f819304d53
Emanuel Avila <76269418+meiazero@users.noreply.github>
2023-06-11 23:10:43 -0300

    style: use loading component

--- src/app/mapa/page.jsx
+++ src/app/mapa/page.jsx
@@ -1,17 +1,21 @@
 "use client"
+import dynamic from "next/dynamic"
 
 import Footer from "../components/Footer"
 import Header from "../components/Header"
-import dynamic from "next/dynamic"
+import Loading from "../components/Loading"
 
-const DynamicMap = dynamic(() => import("../components/Map"), { ssr: false })
+const DynamicMap = dynamic(() => import("../components/Map"), {
+    ssr: false,
+    loading: () => <Loading />
+})
 
 export default function Mapa() {
     return (
-        <>
+        <div>
             <Header />
             <DynamicMap />
             <Footer />
-        </>
+        </div>
     )
 }
